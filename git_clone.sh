df6fc60ef18a7cba0f37f90a5c410a7c7c423903
Brian Cox <45868083+BCI83@users.noreply.github.com>
2024-10-08 16:17:34 +0100

    Update git_clone.sh

--- git_clone.sh
+++ git_clone.sh
@@ -7,6 +7,6 @@ cd /cert-monitor
 docker compose down -v
 cd /
 rm -rf /cert-monitor
-git clone https://github.com/BCI83/Cets_and_Domain_Dashboard.git /cert-monitor
+git clone https://github.com/BCI83/Certs_and_Domain_Dashboard.git /cert-monitor
 cd /cert-monitor
 docker build --network=host -t cert-monitor/cert-monitor:latest .
