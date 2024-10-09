dc4be3e0c0478fbc12ceffc8749baa82521ae0a1
Brian Cox <brian1664@gmail.com>
2024-10-09 13:19:18 +0100

    .

--- git_clone.sh
+++ git_clone.sh
@@ -6,6 +6,7 @@
 cd /cert-monitor
 docker compose down -v
 cd /
+docker rmi $(docker images cert-monitor/cert-monitor:latest -q)
 rm -rf /cert-monitor
 git clone https://github.com/BCI83/Certs_and_Domain_Dashboard.git /cert-monitor
 cd /cert-monitor
