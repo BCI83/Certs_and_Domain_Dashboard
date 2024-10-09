d39e48e922267b6ab975ad6912f4f01d9018410d
Brian Cox <brian1664@gmail.com>
2024-10-09 18:46:56 +0100

    .

--- update_data.py
+++ update_data.py
@@ -87,7 +87,7 @@ def build_html_email(expiring_domains, expiring_ssl):
     """
 
     # Add domain expiries
-    html_content += "<h3>Expiring Domains:</h3>"
+    html_content += "<h3>Domains:</h3>"
     if expiring_domains:
         html_content += "<table><tr><th>Domain</th><th>WHOIS Expiry</th></tr>"
         for domain in expiring_domains:
@@ -99,7 +99,7 @@ def build_html_email(expiring_domains, expiring_ssl):
         html_content += "<p>N/A</p>"
 
     # Add SSL expiries
-    html_content += "<br><br><h3>Expiring SSL Certs:</h3>"
+    html_content += "<br><br><h3>SSL Certs:</h3>"
     if expiring_ssl:
         html_content += "<table><tr><th>Site</th><th>SSL Expiry (YYYY-MM-DD-UTC)</th></tr>"
         for subdomain in expiring_ssl:
